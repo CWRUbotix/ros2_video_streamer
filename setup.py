002c44067858ab56432af4306dc7471bcb922ca1
Michael Carlstrom <36806982+InvincibleRMC@users.noreply.github.com>
2023-02-17 21:04:31 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     keywords=['ROS2'],
     description='Camera simulator - run a recorded video file streamed on a ros topic as if a live webcamera.',
     license='Apache License, Version 2.0',
-    test_suite='test',
+    test_require=['pytest'],
     entry_points={
         'console_scripts': [
             'ros2_video_streamer_node = ros2_video_streamer.ros2_video_streamer_node:main',
