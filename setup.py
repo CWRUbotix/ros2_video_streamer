b5bb6f454ca4f84d1c036d6439ac9b2839051349
Michael Carlstrom <rmc@carlstrom.com>
2023-10-22 12:49:51 -0400

    hope

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     zip_safe=True,
     install_requires=[
         'setuptools',
-        'opencv-python'
+        'opencv-python-headless'
     ],
     author='Benjamin Poulin',
     maintainer='Benjamin Poulin',
