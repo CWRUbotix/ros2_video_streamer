e1a8d332467ed3109117a19db74c2385b59f116a
Michael Carlstrom <rmc@carlstrom.com>
2023-12-07 12:11:27 -0500

    fix submodule

--- test/test_mypy.py
+++ test/test_mypy.py
@@ -11,5 +11,7 @@ def test_mypy() -> None:
     """Tests mypy on this module."""
     file_path = __file__.replace(f'{__name__}.py', '')
     config_file = os.path.join(file_path, '..', '..', '..', '..', 'mypy.ini')
+    if not os.path.exists(config_file):
+        config_file = os.path.join(file_path, '..', 'mypy.ini')
     error_code = main(argv=['--config', config_file])
     assert error_code == 0, 'Found code style errors / warnings'
