943b2633365243e6d19aa0bf32af58f54f5fffa0
Michael Carlstrom <rmc@carlstrom.com>
2024-07-24 14:59:36 -0400

    quotes again

--- launch/ros2_video_streamer_node_launch.py
+++ launch/ros2_video_streamer_node_launch.py
@@ -25,69 +25,69 @@ def launch_setup(
     # Only strictly necessary to set streamer node name & namespace,
     # but makes other substitutions nicer too
     camera_name_argument: DeclareLaunchArgument = DeclareLaunchArgument(
-        "camera_name",
-        default_value="simulated_cam",
-        description="Name of the camera, used to autogenerate"
-        + "`image_topic_name`, `info_topic_name`, & `file_name`. Will find video"
-        + "files named `<camera_name>.mp4`.",
+        'camera_name',
+        default_value='simulated_cam',
+        description='Name of the camera, used to autogenerate'
+        + '`image_topic_name`, `info_topic_name`, & `file_name`. Will find video'
+        + 'files named `<camera_name>.mp4`.',
     )
-    camera_name_str = LaunchConfiguration("camera_name").perform(context)
+    camera_name_str = LaunchConfiguration('camera_name').perform(context)
 
     launch_arguments: list[DeclareLaunchArgument] = [
         DeclareLaunchArgument(
-            "node_name",
-            default_value="streamer_node",
+            'node_name',
+            default_value='streamer_node',
         ),
         DeclareLaunchArgument(
-            "image_topic_name",
-            default_value=f"/{camera_name_str}/image_raw",
+            'image_topic_name',
+            default_value=f'/{camera_name_str}/image_raw',
         ),
         DeclareLaunchArgument(
-            "info_topic_name",
-            default_value=f"/{camera_name_str}/camera_info",
+            'info_topic_name',
+            default_value=f'/{camera_name_str}/camera_info',
         ),
         DeclareLaunchArgument(
-            "config_file_name",
-            default_value="",
-            description="Name of the config file. Defaults to empty string, which"
-            + "means no config file. Contents published on `CameraInfo` message.",
+            'config_file_name',
+            default_value='',
+            description='Name of the config file. Defaults to empty string, which'
+            + 'means no config file. Contents published on `CameraInfo` message.',
         ),
-        DeclareLaunchArgument("loop", default_value="true"),
+        DeclareLaunchArgument('loop', default_value='true'),
         DeclareLaunchArgument(
-            "frame_id",
-            default_value="",
-            description="`frame_id` field in the `CameraInfo` topic",
-        ),
-        DeclareLaunchArgument(
-            "type",
-            description="Type of media source, (e.g. image or video)",
+            'frame_id',
+            default_value='',
+            description='`frame_id` field in the `CameraInfo` topic',
         ),
         DeclareLaunchArgument(
-            "file_name",
-            default_value=f"{camera_name_str}.mp4",
-            description="Name of file",
+            'type',
+            description='Type of media source, (e.g. image or video)',
         ),
         DeclareLaunchArgument(
-            "start",
-            default_value="0",
-            description="Where the Video is starting from default is frame 0",
+            'file_name',
+            default_value=f'{camera_name_str}.mp4',
+            description='Name of file',
+        ),
+        DeclareLaunchArgument(
+            'start',
+            default_value='0',
+            description='Where the Video is starting from default is frame 0',
         ),
     ]
 
     streamer_node: Node = Node(
-        package="ros2_video_streamer",
-        executable="ros2_video_streamer_node",
-        name="streamer_node",
-        namespace=f"simulation/{camera_name_str}",
+        package='ros2_video_streamer',
+        executable='ros2_video_streamer_node',
+        name='streamer_node',
+        namespace=f'simulation/{camera_name_str}',
         parameters=[
-            {"config_file_path": LaunchConfiguration("config_file_name")},
-            {"image_topic_name": LaunchConfiguration("image_topic_name")},
-            {"info_topic_name": LaunchConfiguration("info_topic_name")},
-            {"loop": LaunchConfiguration("loop")},
-            {"frame_id": LaunchConfiguration("frame_id")},
-            {"type": LaunchConfiguration("type")},
-            {"file_name": LaunchConfiguration("file_name")},
-            {"start": LaunchConfiguration("start")},
+            {'config_file_path': LaunchConfiguration('config_file_name')},
+            {'image_topic_name': LaunchConfiguration('image_topic_name')},
+            {'info_topic_name': LaunchConfiguration('info_topic_name')},
+            {'loop': LaunchConfiguration('loop')},
+            {'frame_id': LaunchConfiguration('frame_id')},
+            {'type': LaunchConfiguration('type')},
+            {'file_name': LaunchConfiguration('file_name')},
+            {'start': LaunchConfiguration('start')},
         ],
     )
 
--- launch/three_camera_launch.py
+++ launch/three_camera_launch.py
@@ -12,9 +12,9 @@ def generate_launch_description() -> launch.launch_description.LaunchDescription
 
     You'll need `bottom_cam.mp4`, `front_cam.mp4`, & `manip_cam.mp4` in `ros2_video_streamer`.
     """
-    front_cam_launcher: IncludeLaunchDescription = create_cam_launcher("front")
-    manip_cam_launcher: IncludeLaunchDescription = create_cam_launcher("manip")
-    bottom_cam_launcher: IncludeLaunchDescription = create_cam_launcher("bottom")
+    front_cam_launcher: IncludeLaunchDescription = create_cam_launcher('front')
+    manip_cam_launcher: IncludeLaunchDescription = create_cam_launcher('manip')
+    bottom_cam_launcher: IncludeLaunchDescription = create_cam_launcher('bottom')
 
     return launch.launch_description.LaunchDescription(
         [
@@ -32,31 +32,31 @@ def create_cam_launcher(
         PythonLaunchDescriptionSource(
             [
                 os.path.join(
-                    get_package_share_directory("ros2_video_streamer"),
-                    "launch",
-                    "ros2_video_streamer_node_launch.py",
+                    get_package_share_directory('ros2_video_streamer'),
+                    'launch',
+                    'ros2_video_streamer_node_launch.py',
                 )
             ]
         ),
         # Need to set normally camera_name-derived args manually b/c ROS gets
         # confused and makes them the same when we launch simultaneously
         launch_arguments=[
-            ("type", "video"),
+            ('type', 'video'),
             (
-                "camera_name",
-                f"{camera_name}_cam",
+                'camera_name',
+                f'{camera_name}_cam',
             ),
             (
-                "image_topic_name",
-                f"/{camera_name}_cam/image_raw",
+                'image_topic_name',
+                f'/{camera_name}_cam/image_raw',
             ),
             (
-                "info_topic_name",
-                f"/{camera_name}_cam/camera_info",
+                'info_topic_name',
+                f'/{camera_name}_cam/camera_info',
             ),
             (
-                "file_name",
-                f"{camera_name}_cam.mp4",
+                'file_name',
+                f'{camera_name}_cam.mp4',
             ),
         ],
     )
